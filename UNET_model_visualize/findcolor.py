f811f719f1efa7cf552708963b5ae0398be43b26
Zenodia <Zenodia@users.noreply.github.com>
2019-11-11 12:53:11 +0100

    model interpretability with CONX
    using unet as model architecture to visualize model interpretability when a pre-trained model makes a prediction on incoming dataset
    CONX is a wrapper around keras and keras is a wrapper around tensorflow

---
+++ UNET_model_visualize/findcolor.py
@@ -0,0 +1,35 @@
+# import the necessary packages
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import utils
+import cv2
+
+def find_color(img_path):
+    # load the image and convert it from BGR to RGB so that
+    # we can dispaly it with matplotlib
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # show our image
+    #plt.figure()
+    #plt.axis("off")
+    #plt.imshow(image)
+
+    # reshape the image to be a list of pixels
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+    # cluster the pixel intensities
+    clt = KMeans(n_clusters = 3)
+    clt.fit(image)
+
+    # build a histogram of clusters and then create a figure
+    # representing the number of pixels labeled to each color
+    hist = utils.centroid_histogram(clt)
+    #print(clt.cluster_centers_)
+    bar = utils.plot_colors(hist, clt.cluster_centers_)
+    #print("color centroids",clt.cluster_centers_)
+    # show our color bart
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(bar)
+    plt.show()
